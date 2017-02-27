81b0b0b5551f5ddb7ccd09db2df6b4f784f1b9d5
JacobWylie <jacobwylie@gmail.com>
2017-02-27 17:48:29 +0100

    added quiz_ui functionality

--- quiz.js
+++ quiz.js
@@ -1,20 +1,20 @@
 function Quiz(questions) {
-	this.score = 0;
-	this.questions = questions;
-	this.currentQuestionIndex = 0;
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
 }
 
 Quiz.prototype.guess = function(answer) {
-	if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
-		this.score++;
-	}
-	this.currentQuestionIndex++;
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
 };
 
 Quiz.prototype.getCurrentQuestion = function() {
-	return this.questions[this.currentQuestionIndex];
+    return this.questions[this.currentQuestionIndex];
 };
 
 Quiz.prototype.hasEnded = function() {
-	return this.currentQuestionIndex >= this.questions.length;
-}
+    return this.currentQuestionIndex >= this.questions.length;
+};
